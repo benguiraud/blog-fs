e8df4eab0d387bfb6924f734323591b055994dbd
benguiraud <33058693+benguiraud@users.noreply.github.com>
2017-11-08 16:26:52 +0100

    banner and footer

--- banner.php
+++ banner.php
@@ -1,4 +1,3 @@
 <div class="container-fluid"> 
     <div class="col-md-12"><h1><b>AWESOME BLOG</b></h1></div>
 </div>
-qsdcfvgbnhj,k;lwxdc vbhjn,;lwxc vbnj,
---
+++ footer.php
@@ -0,0 +1,4 @@
+<div class="container-fluid"> 
+    <div class="col-md-12"><p>this blog is a masterpiece please be kind with it.
+    <p>awesomly done by Ben'/&'Naim</p></p></div>
+</div>
