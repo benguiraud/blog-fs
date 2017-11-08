19ca604986ea885630cf653dfe89ebce9e2234c1
benguiraud <33058693+benguiraud@users.noreply.github.com>
2017-11-08 16:57:49 +0100

    css2.0

--- footer.php
+++ footer.php
@@ -1,4 +1,6 @@
 <div class="container-fluid"> 
-    <div class="col-md-12 bg"><p>this blog is a masterpiece please be kind with it.
-    <p>awesomly done by Ben et Naim</p></div>
+    <div class="col-md-12 bg">
+        <p>this blog is a masterpiece please be kind with it.
+        <p>awesomly done by Ben et Naim</p>
+    </div>
 </div>
