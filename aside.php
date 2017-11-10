d5bcd6e29bc3949effb2307a0f9e3a8f12dd8de3
benguiraud <33058693+benguiraud@users.noreply.github.com>
2017-11-10 11:19:16 +0100

    1

---
+++ aside.php
@@ -0,0 +1,50 @@
+<?php
+$titre=
+    
+    
+    foreach ("$titre")
+    echo 
+
+
+
+
+
+
+
+
+echo "<table>";
+
+foreach($titre as $elem) {
+    
+
+ 
+    echo  "<tr><td><a href=' $elem[0]' >".;
+
+
+}
+
+echo "</table>";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
